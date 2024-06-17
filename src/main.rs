893c99705c3235957e51e08010f739b64a8c22e8
James Browning <browningj869@gmail.com>
2024-06-17 13:54:22 -0400

    return failure

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,9 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    match input.trim() {
+        "exit" => return,
+        _ => println!("{}: command not found", input.trim()),
+        
+    }
 }
