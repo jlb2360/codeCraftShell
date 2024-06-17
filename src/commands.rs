671e24166a68c8c0496b6b1fbcdd17ec80ad8d62
James Browning <browningj869@gmail.com>
2024-06-17 14:26:32 -0400

    added builtin type

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,70 @@
 
 
-pub fn exit_command(params: Vec<&str>) {
+struct Command {
+    name: &'static str,
+    function: fn(Vec<&str>),
+}
+
+pub struct Commands {
+    built_in: Vec<Command>,
+}
+
+impl Commands {
+    pub fn new() -> Commands {
+        Commands {
+            built_in: Vec::new(),
+        }
+    }
+
+
+    
+
+    pub fn add_built_in_commands(&mut self) {
+        self.built_in.push(Command {
+            name: "exit",
+            function: exit_command,
+        });
+
+        self.built_in.push(Command {
+            name: "echo",
+            function: echo_command,
+        });
+    }
+
+    pub fn execute_command(&self, name: &str, params: Vec<&str>) {
+        for command in &self.built_in {
+            if command.name == name {
+                (command.function)(params);
+                return;
+            }
+        }
+
+        if name == "type" {
+            self.type_command(params);
+            return;
+        }
+
+        println!("{}: command not found", name);
+    }
+
+    fn type_command(&self, params: Vec<&str>) {
+        let command = params[0];
+        for c in &self.built_in {
+            if c.name == command {
+                println!("{} is a shell builtin", command);
+                return;
+            }
+        }
+        if command == "type" {
+            println!("type is a shell builtin");
+            return;
+        }
+        println!("{} is not a shell builtin", command);
+    }
+    
+}
+
+fn exit_command(params: Vec<&str>) {
     if params.len() > 2 {
         println!("exit: too many arguments");
         return;
@@ -15,4 +79,8 @@ pub fn exit_command(params: Vec<&str>) {
     } else {
         std::process::exit(0);
     }
+}
+
+fn echo_command(params: Vec<&str>) {
+    println!("{}", params.join(" "));
 }
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,23 @@ use std::io::{self, Write};
 
 mod commands;
 
-use commands::exit_command;
+use commands::Commands;
+
 
 fn main() {
     // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut commands = Commands::new();
+    commands.add_built_in_commands();
+
 
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     while stdin.read_line(&mut input).unwrap() > 0 {
         let ls = input.trim().split_whitespace().collect::<Vec<&str>>();
-        match ls[0] {
-            "exit" => exit_command(ls),
-            "echo" => println!("{}", ls[1..].join(" ")),
-            _ => println!("{}: command not found", ls[0]),
-        }
-
+        commands.execute_command(ls[0], ls[1..].to_vec());
         print!("$ ");
         io::stdout().flush().unwrap();
         input.clear();
